bf861174da9da7273c23525df326b007e64e296d
Craig Fox <cwfox@arkbluecross.com>
2021-11-11 12:04:53 -0600

    understanding components video

--- src/App.js
+++ src/App.js
@@ -5,21 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="LOGO!" />
+        <HelloPizza />
       </header>
     </div>
   );
+}
+
+function HelloPizza() {
+  return <h1>Pizza pockets are dope!</h1>
 }
 
 export default App;
