53c53a6ad0f17ca6ef43f19302ce0c31d2a2a0eb
Craig Fox <cwfox@arkbluecross.com>
2021-11-11 12:31:35 -0600

    default props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
+import logo from './pizza.jpg';
 import './App.css';
+import PropTypes from 'prop-types';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="LOGO!" />
+
         <HelloPizza />
+        <HelloPizza name="rolls" adjective="sweet" />
       </header>
     </div>
   );
 }
 
-function HelloPizza() {
-  return <h1>Pizza pockets are dope!</h1>
+function HelloPizza({name = "pockets", adjective = "dope"}) {
+  return (
+    <h1>Pizza <span>{name}</span> are <span>{adjective}</span>!</h1>
+  )
+}
+
+HelloPizza.propTypes = {
+  name: PropTypes.string,
+  adjective: PropTypes.string
 }
 
 export default App;
