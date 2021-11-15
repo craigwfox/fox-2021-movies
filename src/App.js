68813e4ee0dad8ebbc9d9bf9a0c63106691651d9
Craig Fox <cwfox@arkbluecross.com>
2021-11-15 15:36:55 -0600

    looping in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-import { Input } from './Input';
+import { MoviesList } from './MoviesList';
 import PropTypes from 'prop-types';
 
 function App() {
   return (
     <div className="App">
-      <HelloPizza />
-
-      <Input placeholder="stuffy stuff" />
+      <MoviesList />
     </div>
   );
-}
-
-function HelloPizza({name = "pockets", adjective = "dope"}) {
-  return (
-    <h1>Pizza <span>{name}</span> are <span>{adjective}</span>!</h1>
-  )
-}
-
-HelloPizza.propTypes = {
-  name: PropTypes.string,
-  adjective: PropTypes.string
 }
 
 export default App;
---
+++ src/MoviesList.js
@@ -0,0 +1,256 @@
+const movies = [
+  {
+    name: '2 Fast 2 Furious'
+  },
+  {
+    name: 'Abraham Lincoln: Vampire Hunter'
+  },
+  {
+    name: 'An American Pickle'
+  },
+  {
+    name: 'Anette'
+  },
+  {
+    name: 'Army of the Dead'
+  },
+  {
+    name: 'Bad Education'
+  },
+  {
+    name: 'Big Eyes'
+  },
+  {
+    name: 'Birdman'
+  },
+  {
+    name: 'Black Widow'
+  },
+  {
+    name: 'Bladerunner'
+  },
+  {
+    name: 'Blood Red Sky'
+  },
+  {
+    name: 'Captain Fantastic'
+  },
+  {
+    name: 'Cats (2018)'
+  },
+  {
+    name: 'Come Away'
+  },
+  {
+    name: 'Cruellella'
+  },
+  {
+    name: 'Dark Phoenix'
+  },
+  {
+    name: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb'
+  },
+  {
+    name: 'Enola Holmes'
+  },
+  {
+    name: 'Fast and Furious (2009)'
+  },
+  {
+    name: 'Fast and the Furious (2001)'
+  },
+  {
+    name: 'Fear Street: Part One - 1994'
+  },
+  {
+    name: 'Fear Street: Part Three - 1666'
+  },
+  {
+    name: 'Fear Street: Part Two - 1978'
+  },
+  {
+    name: 'Ford vs Ferrari'
+  },
+  {
+    name: 'Ghostbusters (1984)'
+  },
+  {
+    name: 'Godzilla (2014)'
+  },
+  {
+    name: 'Godzilla vs. Kong'
+  },
+  {
+    name: 'Godzilla: King of the Monsters'
+  },
+  {
+    name: 'Gunpowder Milkshake'
+  },
+  {
+    name: 'Guns Akimbo'
+  },
+  {
+    name: 'Horns'
+  },
+  {
+    name: 'Hotel Coppelia'
+  },
+  {
+    name: 'I Care a Lot'
+  },
+  {
+    name: 'Intersteller'
+  },
+  {
+    name: 'Just Mercy'
+  },
+  {
+    name: 'Kill Bill Vol 1'
+  },
+  {
+    name: 'Knight\'s of Badassdom'
+  },
+  {
+    name: 'Kong: Skull Island'
+  },
+  {
+    name: 'Ma Rainey\'s Black Bottom'
+  },
+  {
+    name: 'Midnight Sky'
+  },
+  {
+    name: 'Midway'
+  },
+  {
+    name: 'Miles Davis: The Birth of Cool'
+  },
+  {
+    name: 'Minari'
+  },
+  {
+    name: 'Moxie'
+  },
+  {
+    name: 'News of the World'
+  },
+  {
+    name: 'One Hour Photo'
+  },
+  {
+    name: 'One Night in Miami '
+  },
+  {
+    name: 'Rememory'
+  },
+  {
+    name: 'Reminiscence'
+  },
+  {
+    name: 'Saving Mr. Banks'
+  },
+  {
+    name: 'Sherlock Holmes'
+  },
+  {
+    name: 'Shirley'
+  },
+  {
+    name: 'Sonic the Hedgehog'
+  },
+  {
+    name: 'The Birds of Prey'
+  },
+  {
+    name: 'The Danish Girl'
+  },
+  {
+    name: 'The Dead Don\'t Die'
+  },
+  {
+    name: 'The Devil All the Time'
+  },
+  {
+    name: 'The Dig'
+  },
+  {
+    name: 'The Disaster Artist'
+  },
+  {
+    name: 'The Great Debaters'
+  },
+  {
+    name: 'The Green Knight'
+  },
+  {
+    name: 'The Guilty'
+  },
+  {
+    name: 'The Lighthouse'
+  },
+  {
+    name: 'The Little Things'
+  },
+  {
+    name: 'The Many Saints of Newark'
+  },
+  {
+    name: 'The Polka King'
+  },
+  {
+    name: 'The Report'
+  },
+  {
+    name: 'The Room'
+  },
+  {
+    name: 'The Suicide Squad'
+  },
+  {
+    name: 'The Theory of Everything'
+  },
+  {
+    name: 'The Trial of the Chicago 7'
+  },
+  {
+    name: 'The Vault'
+  },
+  {
+    name: 'The Woman in the Window'
+  },
+  {
+    name: 'Thunder Force'
+  },
+  {
+    name: 'War Dogs'
+  },
+  {
+    name: 'Judas and the Black Messiah'
+  },
+  {
+    name: 'Nobody'
+  },
+  {
+    name: 'Red Notice'
+  },
+  {
+    name: 'Les Miserables'
+  },
+  {
+    name: 'Kajillionaire'
+  }
+]
+
+export function MoviesList() {
+  return (
+    <div>
+      <ul className="movies-list">
+        { movies.map((movie) => {
+          return (
+            <li key={movie.name}>{movie.name}</li>
+          )
+        }) }
+      </ul>
+    </div>
+  )
+}
