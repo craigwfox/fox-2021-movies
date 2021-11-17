a31084086f360a596009c7e51a5da14c1913303f
Craig Fox <cwfox@arkbluecross.com>
2021-11-16 22:57:40 -0600

    hitting an apit

--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -2,255 +2,26 @@ import { useState, useEffect } from "react"
 import { Movie } from "./Movie"
 import { Filter } from "../Filter"
 
-const movies = [
-  {
-    name: '2 Fast 2 Furious'
-  },
-  {
-    name: 'Abraham Lincoln: Vampire Hunter'
-  },
-  {
-    name: 'An American Pickle'
-  },
-  {
-    name: 'Anette'
-  },
-  {
-    name: 'Army of the Dead'
-  },
-  {
-    name: 'Bad Education'
-  },
-  {
-    name: 'Big Eyes'
-  },
-  {
-    name: 'Birdman'
-  },
-  {
-    name: 'Black Widow'
-  },
-  {
-    name: 'Bladerunner'
-  },
-  {
-    name: 'Blood Red Sky'
-  },
-  {
-    name: 'Captain Fantastic'
-  },
-  {
-    name: 'Cats (2018)'
-  },
-  {
-    name: 'Come Away'
-  },
-  {
-    name: 'Cruellella'
-  },
-  {
-    name: 'Dark Phoenix'
-  },
-  {
-    name: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb'
-  },
-  {
-    name: 'Enola Holmes'
-  },
-  {
-    name: 'Fast and Furious (2009)'
-  },
-  {
-    name: 'Fast and the Furious (2001)'
-  },
-  {
-    name: 'Fear Street: Part One - 1994'
-  },
-  {
-    name: 'Fear Street: Part Three - 1666'
-  },
-  {
-    name: 'Fear Street: Part Two - 1978'
-  },
-  {
-    name: 'Ford vs Ferrari'
-  },
-  {
-    name: 'Ghostbusters (1984)'
-  },
-  {
-    name: 'Godzilla (2014)'
-  },
-  {
-    name: 'Godzilla vs. Kong'
-  },
-  {
-    name: 'Godzilla: King of the Monsters'
-  },
-  {
-    name: 'Gunpowder Milkshake'
-  },
-  {
-    name: 'Guns Akimbo'
-  },
-  {
-    name: 'Horns'
-  },
-  {
-    name: 'Hotel Coppelia'
-  },
-  {
-    name: 'I Care a Lot'
-  },
-  {
-    name: 'Intersteller'
-  },
-  {
-    name: 'Just Mercy'
-  },
-  {
-    name: 'Kill Bill Vol 1'
-  },
-  {
-    name: 'Knight\'s of Badassdom'
-  },
-  {
-    name: 'Kong: Skull Island'
-  },
-  {
-    name: 'Ma Rainey\'s Black Bottom'
-  },
-  {
-    name: 'Midnight Sky'
-  },
-  {
-    name: 'Midway'
-  },
-  {
-    name: 'Miles Davis: The Birth of Cool'
-  },
-  {
-    name: 'Minari'
-  },
-  {
-    name: 'Moxie'
-  },
-  {
-    name: 'News of the World'
-  },
-  {
-    name: 'One Hour Photo'
-  },
-  {
-    name: 'One Night in Miami '
-  },
-  {
-    name: 'Rememory'
-  },
-  {
-    name: 'Reminiscence'
-  },
-  {
-    name: 'Saving Mr. Banks'
-  },
-  {
-    name: 'Sherlock Holmes'
-  },
-  {
-    name: 'Shirley'
-  },
-  {
-    name: 'Sonic the Hedgehog'
-  },
-  {
-    name: 'The Birds of Prey'
-  },
-  {
-    name: 'The Danish Girl'
-  },
-  {
-    name: 'The Dead Don\'t Die'
-  },
-  {
-    name: 'The Devil All the Time'
-  },
-  {
-    name: 'The Dig'
-  },
-  {
-    name: 'The Disaster Artist'
-  },
-  {
-    name: 'The Great Debaters'
-  },
-  {
-    name: 'The Green Knight'
-  },
-  {
-    name: 'The Guilty'
-  },
-  {
-    name: 'The Lighthouse'
-  },
-  {
-    name: 'The Little Things'
-  },
-  {
-    name: 'The Many Saints of Newark'
-  },
-  {
-    name: 'The Polka King'
-  },
-  {
-    name: 'The Report'
-  },
-  {
-    name: 'The Room'
-  },
-  {
-    name: 'The Suicide Squad'
-  },
-  {
-    name: 'The Theory of Everything'
-  },
-  {
-    name: 'The Trial of the Chicago 7'
-  },
-  {
-    name: 'The Vault'
-  },
-  {
-    name: 'The Woman in the Window'
-  },
-  {
-    name: 'Thunder Force'
-  },
-  {
-    name: 'War Dogs'
-  },
-  {
-    name: 'Judas and the Black Messiah'
-  },
-  {
-    name: 'Nobody'
-  },
-  {
-    name: 'Red Notice'
-  },
-  {
-    name: 'Les Miserables'
-  },
-  {
-    name: 'Kajillionaire'
-  }
-]
+const API_URL = ' https://api.themoviedb.org/3/discover/movie?api_key=***REMOVED***&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
 
 export function MoviesList() {
   const [filter, setFilter] = useState("")
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async () => {
+    try {
+      const res = await fetch(API_URL)
+      const movies = await res.json()
+
+      setMovies(movies.results)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   useEffect(() => {
-    console.log('hit effect')
-  }, [filter])
+    getMovies()
+  }, [])
 
   return (
     <div className="movies-list">
@@ -258,9 +29,9 @@ export function MoviesList() {
 
       <ul>
         { movies.filter((movie) =>
-            movie.name.toLowerCase().includes(filter.toLowerCase())
+            movie.title.toLowerCase().includes(filter.toLowerCase())
           ).map((movie) => (
-            <Movie key={movie.name} movie={movie} />
+            <Movie key={movie.id} movie={movie} />
           )
         )}
       </ul>
