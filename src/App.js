69d50102526dd244804ae1725cabd2d4e755a494
Craig Fox <cwfox@arkbluecross.com>
2021-11-18 10:58:59 -0600

    added movie details component and base routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import { MoviesList } from './movies/MoviesList';
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import { MoviesList } from './movies/MoviesList'
+import { MovieDetail } from './movies/MovieDetail'
 
 function App() {
   return (
-    <main id="maincontent" className="App">
-      <h1>The Foxy movies of 2021 🍿</h1>
-      <MoviesList />
-    </main>
+    <div className="page-wrapper">
+      <header className="site-header">
+        <h1>The Foxy movies of 2021 🍿</h1>
+      </header>
+      <main id="maincontent" className="App main">
+        <Router>
+          <Routes>
+            <Route path="/" element={<MoviesList />} />
+            <Route path="/details" element={<MovieDetail />} />
+          </Routes>
+        </Router>
+      </main>
+    </div>
   );
 }
 
