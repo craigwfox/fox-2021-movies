e95e3a4e1b468377f45d05e615b835bf95f7d586
Craig Fox <cwfox@arkbluecross.com>
2021-11-19 07:23:16 -0600

    Added detail page content

--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -4,10 +4,12 @@ import { Header } from "../Header"
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const API_KEY = '?api_key=***REMOVED***'
+const CONFIG_URL = 'https://api.themoviedb.org/3/configuration?api_key=***REMOVED***'
 
 // {config, extras}
 export function MovieDetail() {
   const { id } = useParams()
+  const [config, setConfig] = useState({})
   const [ movie, setMovie ] = useState({})
 
   const getMovie = async () => {
@@ -21,7 +23,19 @@ export function MovieDetail() {
     }
   }
 
+  const getConfig = async () => {
+    try {
+      const res = await fetch(CONFIG_URL)
+      const config = await res.json()
+
+      setConfig(config)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   useEffect(() => {
+    getConfig()
     getMovie()
   }, [id])
 
@@ -31,15 +45,38 @@ export function MovieDetail() {
   return (
     <div className="page-wrapper">
       <Header />
-      <main id="maincontent" className="App main">
-        <div className="movies-single">
-          <ul>
+      <main id="maincontent" className="App main main--detail">
+        <div className="detail__backdrop">
+          {config.images?.secure_base_url && (
+            <img
+              aria-hidden="true"
+              src={config.images.secure_base_url + config.images.backdrop_sizes[2] + movie.backdrop_path}
+              alt={`Poster for ${movie.title}`} />
+          )}
+        </div>
+        <div className="detail">
+          <h1 className="detail__title">{ movie.title }</h1>
+          <ul className="detail__genres">
             {movie.genres.map( (genre) => (
               <li key={ genre.name }>{ genre.name }</li>)
             )}
           </ul>
-          <h1>{ movie.title }</h1>
-          <p>{ movie.overview }</p>
+          <div className="detail__content">
+            <ul className="detail__stats">
+              <li><strong>Release:</strong> { movie.release_date }</li>
+              <li><strong>Runtime</strong> { movie.runtime }</li>
+              <li><strong>Rating:</strong> { movie.vote_average } / 10</li>
+              <li><strong>IMDB:</strong> { movie.imdb_id }</li>
+            </ul>
+            <h2 className="detail__tagline">{ movie.tagline }</h2>
+            <p>{ movie.overview }</p>
+          </div>
+          {config.images?.secure_base_url && (
+            <img
+              className="detail__poster"
+              src={config.images.secure_base_url + config.images.poster_sizes[4] + movie.poster_path}
+              alt={`Poster for ${movie.title}`} />
+          )}
         </div>
       </main>
     </div>
